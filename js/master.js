91aef5c2a76c8e29816c8686169bc06ce581d37f
Ryan Murry <ryanjmurry@gmail.com>
2018-02-25 08:49:37 -0800

    add javascript to show only certain portions of the page based on user input.

---
+++ js/master.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#questions form").submit(function(event) {
+    var work = parseInt($("#work").val());
+    var trip = parseInt($("#trip").val());
+    var dinner = parseInt($("#trip").val());
+    var morning = parseInt($("#trip").val());
+    var eggs = parseInt($("#trip").val());
+    var total = work + trip + dinner + morning + eggs;
+    if (total === 0) {
+      $("#zero").show();
+    } else if (total > 0 && total <= 5) {
+      $("#endurances").show();
+    } else if (total > 5 && total <=10) {
+      $("#tricks"),show();
+    } else if (total > 10 && total <=15) {
+      $("#games").show();
+    } else if (total > 15 && total <=20) {
+      $("#downhills").show();
+    } else {
+      $("#races").show();
+    }
+    event.preventDefault();
+  });
+});
