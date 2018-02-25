85e44d10e66497e598dd5131d47e7ea7a140c388
Ryan Murry <ryanjmurry@gmail.com>
2018-02-25 09:47:02 -0800

    fix bug issue that would result if users total was between 5 and 10.

--- js/master.js
+++ js/master.js
@@ -1,23 +1,21 @@
-
 $(document).ready(function() {
-
-  var nameInput = prompt("What is your first name?");
+  var nameInput = prompt("What is your first name");
   var nameCaps = nameInput.toUpperCase().charAt(0) + nameInput.substr(1);
   $(".username").text(nameCaps);
 
   $("#questions form").submit(function(event) {
     var work = parseInt($("#work").val());
     var trip = parseInt($("#trip").val());
-    var dinner = parseInt($("#trip").val());
-    var morning = parseInt($("#trip").val());
-    var eggs = parseInt($("#trip").val());
+    var dinner = parseInt($("#dinner").val());
+    var morning = parseInt($("#morning").val());
+    var eggs = parseInt($("#eggs").val());
     var total = work + trip + dinner + morning + eggs;
     if (total === 0) {
       $("#zero").show();
     } else if (total > 0 && total <= 5) {
       $("#endurances").show();
     } else if (total > 5 && total <=10) {
-      $("#tricks"),show();
+      $("#tricks").show();
     } else if (total > 10 && total <=15) {
       $("#games").show();
     } else if (total > 15 && total <=20) {
